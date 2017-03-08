8b25732e339decde7b6cd99ca89cca2adcc3a590
Stephen <stephengroat@users.noreply.github.com>
2017-03-08 09:00:06 -0800

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -18,38 +18,9 @@ QIRA_PID=$!
 echo "qira pid is $QIRA_PID"
 sleep 2
 
-LSB="/etc/lsb-release"
-VER="12.04"
-LIBICU="libicu48"
-
-if [ -f $LSB ] ; then
-  echo "*** Debian Based Distro."
-  . $LSB
-  if [ $DISTRIB_ID == "Ubuntu" ]; then
-    if [ $DISTRIB_RELEASE != "12.04" ]; then
-      VER="14.04"
-      LIBICU="libicu52"
-    fi
-  fi
-fi
-
 # phantomjs
 # use phantomjs2.0 for non-draft WebSockets protol
-# unforunately this doesn't ship with Ubuntu by default
 
-unamestr=`uname`
-if [[ "$unamestr" == 'Linux' ]]; then
-  sudo apt-get install $LIBICU
-
-  wget https://s3.amazonaws.com/travis-phantomjs/phantomjs-2.0.0-ubuntu-$VER.tar.bz2
-  tar xf ./phantomjs-2.0.0-ubuntu-$VER.tar.bz2
-  chmod +x ./phantomjs
-  ./phantomjs qira_tests/load_page.js
-elif [[ "$unamestr" == 'Darwin' ]]; then
-  brew install icu4c phantomjs
-  phantomjs qira_tests/load_page.js
-else
-  echo Error
-fi
+phantomjs qira_tests/load_page.js
 
 kill $QIRA_PID
